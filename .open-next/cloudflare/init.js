2a84d675f77f26b74944221a26894d1486402337
jatindecoded <jatinbhasin2012@gmail.com>
2026-02-15 18:04:06 +0530

    improve UI and products page with wrangler build files

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -49,7 +49,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1771110164557,
+    __BUILD_TIMESTAMP_MS__: 1771158803520,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
